e5220fdae6403319ed924baf7245245caefb8032
Verity <83372423+VerityIncorporated@users.noreply.github.com>
2025-01-07 14:17:36 +0000

    Removed unused features in dependencies

--- src/errors/mod.rs
+++ src/errors/mod.rs
@@ -1,7 +1,9 @@
 pub mod read_memory;
 pub mod write_memory;
+#[cfg(feature = "aob")]
 pub mod aob_scan;
 
 pub use read_memory::ReadMemoryError;
 pub use write_memory::WriteMemoryError;
+#[cfg(feature = "runtime")]
 pub use aob_scan::AobScanError;
